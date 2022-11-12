413ce284ee9296d0e8e24af20157faf8d751834f
Roberto Jimenez <robertojimenezg@gmail.com>
2022-11-12 01:32:55 -0600

    rutas delete, put y patch

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -81,4 +81,49 @@ router.post('/', (req, res) => {
   });
 });
 
+/**
+ * Es muy parecido al post, la única diferencia es que se recibe el id del producto que quiero editar
+ *
+ * El put, en vez de patch debería funcionar también, solo que por convención usamos el patch
+ *
+ * http://localhost:3000/api/v1/products/1212
+ *
+ * Se envia:
+ * {
+	"name": "Change Product"
+}
+
+Se recive:
+
+{
+	"message": "update",
+	"data": {
+		"name": "Change Product"
+	},
+	"id": "1212"
+}
+ */
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id: id
+  });
+});
+
+
+/**
+ * El delete recibe un parámetro pero no va a tener un cuerpo, solamente se manda un identificador para ser eliminado
+ */
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'delete',
+    id: id
+  });
+});
+
+
 module.exports = router;
