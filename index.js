76f6f9bdbd29d7600c3268a8d66da305af0262fb
Roberto Jimenez <robertojimenezg@gmail.com>
2022-11-29 17:03:32 -0600

    Solucionando problems de cors

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes/index');
 const app = express();
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 const port =  3000;
 
 app.use(express.json()); // Con esto nos permite recibir inf json desde el post
+// app.use(cors()); // Utilizandolo de esta manera habilita a cualquier dominio
+
+// Si se quiere controlar el acceso a solo algunas aplicaciones se hace de la siguiente manera
+const whiteList = ['http://localhost:8080', 'https://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if(whiteList.includes(origin)){
+      callback(null, true);
+    }else{
+      callback(new Error('No permitido!'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola, mi server en Express...');
